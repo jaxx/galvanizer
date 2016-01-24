214380a780ef1cbe30431e918b096432f8367836
Janno Põldma <janno.poldma@gmail.com>
2016-01-25 00:39:26 +0200

    Use Grunt+Bower+Npm to manage third party libraries.

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,33 @@ extern crate daemon;
 #[macro_use]
 extern crate nickel;
 extern crate config;
+extern crate mustache;
 
-
+use std::collections::HashMap;
 use std::sync::mpsc::Receiver;
 use std::path::Path;
 use std::error::Error;
 use daemon::{Daemon, DaemonRunner, State};
-use nickel::{Nickel, HttpRouter, StaticFilesHandler};
+use nickel::{Nickel, HttpRouter, StaticFilesHandler, Request, Response, MiddlewareResult};
 use config::reader as config_reader;
 use config::types::Config;
+use mustache::Template;
 
 use std::env;
+
+fn render_to_string(template: &Template, data: &mut HashMap<&str, String>) -> String {
+    let mut bytes = vec![];
+    template.render(&mut bytes, data).unwrap();
+    String::from_utf8(bytes).unwrap()
+}
+
+fn show_index<'a>(_: &mut Request, res: Response<'a>) -> MiddlewareResult<'a> {
+    let template = mustache::compile_path(Path::new("app/assets/templates/index.html")).unwrap();
+    let mut data = HashMap::<&str, String>::new();
+    let value = render_to_string(&template, &mut data);
+    data.insert("content", value);
+    res.render("app/assets/templates/layout.html", &data)
+}
 
 fn main() {
     env_logger::init().unwrap();
@@ -45,11 +61,13 @@ fn main() {
                     debug!("main: Service -> start().");
 
                     let mut server = Nickel::new();
+                    let mut router = Nickel::router();
+                    
+                    router.get("/", show_index);
 
-                    server.utilize(StaticFilesHandler::new("assets"));
-                    server.utilize(StaticFilesHandler::new("assets/templates"));
+                    server.utilize(router);
+                    server.utilize(StaticFilesHandler::new("public/assets"));
 
-                    server.get("**", middleware!("Hello world!"));
                     server.listen("127.0.0.1:3000");
                 },
                 State::Reload => debug!("main: Service -> restart()."),
